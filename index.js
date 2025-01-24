b38be1f00db84b56154e6bf917d21df0190cf98e
Prithvi Singh <prithvisingh@Prithvis-MacBook-Air.local>
2025-01-24 15:46:21 +0530

    added the file to the repo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { resolve } = require('path');
 
+
 const app = express();
+require('dotenv').config();
 const port = 3010;
 
 app.use(express.static('static'));
